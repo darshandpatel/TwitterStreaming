698a72fddea64a068e39aaf7430ecde0982518ca
Darshan Patel <patel.dars@husky.neu.edu>
2016-12-26 23:26:25 -0500

    Kafka Tweet producer finished

--- build.sbt
+++ build.sbt
@@ -4,7 +4,11 @@ version := "1.0"
 
 scalaVersion := "2.12.1"
 
-libraryDependencies += "org.twitter4j" % "twitter4j-core" % "2.2.1"
+// https://mvnrepository.com/artifact/org.twitter4j/twitter4j-core
+libraryDependencies += "org.twitter4j" % "twitter4j-core" % "4.0.2"
+
+// https://mvnrepository.com/artifact/org.twitter4j/twitter4j-stream
+libraryDependencies += "org.twitter4j" % "twitter4j-stream" % "4.0.6"
 
 // https://mvnrepository.com/artifact/org.apache.kafka/kafka_2.11
 libraryDependencies += "org.apache.kafka" % "kafka_2.11" % "0.10.1.1"
--- src/main/scala/producer/TweetProducer.scala
+++ src/main/scala/producer/TweetProducer.scala
@@ -1,8 +1,15 @@
 package producer
 
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import java.util.Properties
+import java.util.concurrent.LinkedBlockingQueue
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import twitter4j._
+import twitter4j.conf.ConfigurationBuilder
+
+import scala.collection.JavaConversions._
 import scala.io.Source
+
 /**
   * Created by Darshan on 12/26/16.
   */
@@ -19,10 +26,10 @@ object TweetProducer {
     props.put("metadata.broker.list", brokers)
     props.put("bootstrap.servers", brokers)
     props.put("acks", "all")
-    props.put("retries", 0)
-    props.put("batch.size", 16384)
-    props.put("linger.ms", 1)
-    props.put("buffer.memory", 33554432)
+    //props.put("retries", 0)
+    //props.put("batch.size", 16384)
+    //props.put("linger.ms", 10)
+    //props.put("buffer.memory", 33554432)
 
     props.put("client.id", "TweetProducer")
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
@@ -32,20 +39,89 @@ object TweetProducer {
 
   }
 
-  
+  def buildTwitterStream(twitterConfig : List[String], queue : LinkedBlockingQueue[Status]) = {
+
+    val consumerKey = twitterConfig(0)
+    val consumerSecret = twitterConfig(1)
+    val accessToken = twitterConfig(2)
+    val accessTokenSecret = twitterConfig(3)
+
+    val cb = new ConfigurationBuilder()
+
+    cb.setDebugEnabled(true)
+      .setOAuthConsumerKey(consumerKey.trim())
+      .setOAuthConsumerSecret(consumerSecret.trim())
+      .setOAuthAccessToken(accessToken.trim())
+      .setOAuthAccessTokenSecret(accessTokenSecret.trim())
+
+    /*
+    // Search tweets
+    val tf = new TwitterFactory(cb.build())
+    var twitter = tf.getInstance()
+
+    val query = new Query("merry christmas")
+    query.setCount(2)
+    val result = twitter.search(query)
+    result.getTweets().toList.foreach{ node => println(node)}
+    */
+
+    // Build Twitter Stream
+    val listener = buildStatusListener(queue)
+    val twitterStream = new TwitterStreamFactory(cb.build()).getInstance()
+    twitterStream.addListener(listener)
+
+    val query = new FilterQuery()
+    query.track("merry christmas")
+    twitterStream.filter(query)
+    twitterStream
+  }
+
+  def buildStatusListener(queue : LinkedBlockingQueue[Status]) : StatusListener = {
+
+    val listener = new StatusListener {
+
+      override def onStatus(status:Status) {
+        queue.put(status)
+      }
+
+      override def onDeletionNotice(notice : StatusDeletionNotice) {}
+      override def onTrackLimitationNotice(numOfLimitStatues : Int) {}
+      override def onException(ex : Exception) { ex.printStackTrace }
+
+      override def onStallWarning(warning: StallWarning): Unit = ???
+
+      override def onScrubGeo(userId: Long, upToStatusId: Long): Unit = ???
+    }
+    listener
+  }
 
   def main(args: Array[String]): Unit = {
 
     // Read configuration through command line arguments
-    val events = args(0).toInt
-    val topic = args(1)
-    val brokers = args(2)
-    val filePath = args(3)
+    val topic = args(0)
+    val brokers = args(1)
+    val filePath = args(2)
+
+    val queue = new LinkedBlockingQueue[Status](1000)
 
     // Read Twitter API config
     val twitterConfig = Source.fromFile(filePath).getLines.toList
-
     val producer = TweetProducer.createKafkaProducer(brokers)
+    val listener = TweetProducer.buildTwitterStream(twitterConfig, queue)
+    var index : Int = 0
 
+    while(true){
+
+      val tweet = queue.poll()
+      if(tweet == null){
+        Thread.sleep(1000)
+      }else{
+        for(hashtag <- tweet.getHashtagEntities){
+          println(hashtag.getText)
+          producer.send(new ProducerRecord[String, String](topic, Integer.toString(index), hashtag.getText))
+          index += 1
+        }
+      }
+    }
   }
 }
